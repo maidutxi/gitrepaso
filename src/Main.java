b071a1ed609ba786c820f6cb2680fdc2415b28ff
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:34 +0100

    He añadido 3 lineas

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("He borrado una linea");
+        System.out.println("Mi perro se llama Luna");
+        System.out.println("Me gusta el mar");
+        System.out.println("me gusta la fiesta");
     }
 }
